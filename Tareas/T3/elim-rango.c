6ef2e46897c437d5e75d1c1cf5582fc5d89b59f2
CaldeCrack <an.caldeguardia@gmail.com>
2023-04-27 22:50:56 -0400

    almost done

--- Tareas/T3/elim-rango.c
+++ Tareas/T3/elim-rango.c
@@ -4,5 +4,28 @@
 #include "elim-rango.h"
 
 void eliminarRango(Nodo **phead, double y, double z) {
-  ... complete ...
+  int lower = 0, greater = 1;
+  Nodo *first, *last;
+  printf("x: %f\ny: %f\n", y, z);
+  while((*phead) -> x < y){
+    lower = 1;
+    first = *phead;
+    Nodo *head = *phead;
+    *phead = head -> prox;
+  }
+  while((*phead) -> x <= z){
+    printf("phead: %i\n", (*phead) -> x);
+    Nodo *head = *phead;
+    *phead = head -> prox;
+    free(head);
+    last = *phead;
+    if(*phead == NULL){
+      greater = 0;
+      break;
+    }
+  }
+  if(lower && greater){
+    *phead = first;
+    (*phead) -> prox = last;
+  }
 }
