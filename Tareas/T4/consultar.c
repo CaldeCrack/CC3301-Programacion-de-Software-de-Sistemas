dcbd360e0b292f40b6de64fd62a46c2ad53c40a7
CaldeCrack <an.caldeguardia@gmail.com>
2023-05-23 00:53:59 -0400

    finished

--- Tareas/T4/consultar.c
+++ Tareas/T4/consultar.c
@@ -130,41 +130,51 @@ int main(int argc, char *argv[]) {
   }
 
   // lines greater than the limit
-  // if(lenLine>MAXTAM){
-  //   fprintf(stderr, "%s: el tamaño de las lineas excede el maximo permitido", arch);
-  //   exit(2);
-  // }
+  if(lenLine>MAXTAM){
+    fprintf(stderr, "%s: el tamaño de las lineas excede el maximo permitido", arch);
+    exit(1);
+  }
 
   // file length
   fseek(fileDicc, 0, SEEK_END);
   int lenArch = ftell(fileDicc);
-  // archive not a multiple of the first line length
-  // if(!lenArch%lenLine){
-  //   fprintf(stderr, "%s: el tamaño del archivo no es multiplo del tamaño de la linea", arch);
-  //   exit(3);
-  // }
   int lines = lenArch/lenLine;
   int n = hash_string(key)%lines, j = lines;
 
+  // archive not a multiple of the first line length
+  if(lenArch%lenLine){
+    fprintf(stderr, "%s: el tamaño del archivo no es multiplo del tamaño de la linea\n", arch);
+    exit(3);
+  }
+
   // read file
   while(j--){
-    // read line
-    fseek(fileDicc, n*lenLine, SEEK_SET);
-    int lineLength = ftell(fileDicc);
-    // different line length
-    if(lineLength!=lenLine){
-      // fprintf(stderr, "%s: linea %i de tamaño incorrecto\n", arch, n);
-      printf("%i\n", lineLength);
+    // line of different size
+    int l = 0;
+    if (fgets(buff, MAXTAM, fileDicc)) {
+        while (buff[l] != '\n' && buff[l] != '\0') l++;
+    }
+    if(++l != lenLine && l != 1){
+      fprintf(stderr, "%s: linea %i de tamaño incorrecto\n", arch, n-1);
       exit(1);
     }
 
+    // read line
     fseek(fileDicc, n*lenLine, SEEK_SET);
     fread(buff, lenLine, 1, fileDicc);
-    int rc = strncmp(key, buff, lenKey);
 
+    // compare keys
+    int rc = strncmp(key, buff, lenKey);
 
     // same keys
     if(!rc){
+      // does not have the : character after the key
+      if(buff[lenKey] != ':'){
+        fprintf(stderr, "%s: linea %i no posee : para terminar la llave\n", arch, n);
+        exit(1);
+      }
+
+      // without errors the key is in the file
       fseek(fileDicc, n*lenLine+lenKey+1, SEEK_SET);
       fgets(buff, lenLine, fileDicc);
       fprintf(stdout, buff);
