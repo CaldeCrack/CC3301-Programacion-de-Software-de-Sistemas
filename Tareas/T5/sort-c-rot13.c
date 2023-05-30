3cd37a0a900fb0982832a62ca41f1f9c1842f47e
CaldeCrack <an.caldeguardia@gmail.com>
2023-05-29 22:52:46 -0400

    strCmp ready

--- Tareas/T5/sort-c-rot13.c
+++ Tareas/T5/sort-c-rot13.c
@@ -1,37 +1,23 @@
 #include <string.h>
 
 int strCmp(char *s1, char *s2) {
-    char c1;
-    char c2;
-
-    for (;;) {
-      c1= *s1;
-      c2= *s2;
-
-      if (c1==0)
-        break;
-
-      if (c1!=c2)
-        break;
-
-      s1++;
-      s2++;
+    char c1, c2;
+    char conv[26] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+    while(*s1 || *s2){
+      c1=*s1; c2=*s2;
+      if('a'<=c1 && c1<='z') c1=conv[c1-'a'];
+      if('a'<=c2 && c2<='z') c2=conv[c2-'a'];
+      if(c1!=c2) return c1-c2;;
+      s1++; s2++;
     }
-
-    return c1 - c2;
+    return c1-c2;
 }
 
 void sort(char **a, int n) {
   char **ult= &a[n-1];
   char **p= a;
   while (p<ult) {
-    // No modifique nada arriba de esta linea
-    // Inicio de la parte que debe cambiar
-
     int t1= strCmp(p[0], p[1]);
-
-    // Fin de la parte que debe cambiar
-    // No Cambie nada mas a partir de aca
     if (t1 <= 0)
       p++;
     else {
