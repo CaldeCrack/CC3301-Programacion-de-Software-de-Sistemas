a0bc513d09edc2c2a0417cd474ce4c8adcae4f0f
CaldeCrack <an.caldeguardia@gmail.com>
2023-07-09 20:32:04 -0400

    xd

--- Tareas/T8/maleta.c
+++ Tareas/T8/maleta.c
@@ -13,30 +13,46 @@
 
 // Use la funcion leer para obtener los resultados del pipe
 int leer(int fd, void *vbuf, int n) {
-  char *buf= vbuf;
-  do {
+	char *buf= vbuf;
+	do {
     int rc= read(fd, buf, n);
     if (rc<=0)
-      return 1; /* fracaso: error o fin del archivo/pipe/socket */
+		return 1; /* fracaso: error o fin del archivo/pipe/socket */
     n-= rc; /* descontamos los bytes leídos */
     buf+= rc; /* avanzamos el buffer para no reescribir lo leido previamente */
-  } while (n>0); /* mientras no leamos todo lo que esperamos */
-  return 0; /* exito */
+	} while (n>0); /* mientras no leamos todo lo que esperamos */
+	return 0; /* exito */
 }
 
-double llenarMaleta(double w[], double v[], int z[], int n,
-                    double maxW, int k) {
-  // ... Modifique esta funcion ...
-  // Use fork() para crear 8 nuevos procesos.  El padre solo obtiene los
-  // resultados de los procesos hijos a traves de pipes, entierra a los
-  // los hijos y calcula el resultado final.
-  // Base su solucion en el ejemplo que determina un factor de
-  // un entero, visto en clase auxiliar.
-  // Despues invocar fork() agregue: srandom(getUSecsOfDay()*getpid());
-  // Esto es para que cada proceso genere secuencias de numeros aleatorios
-  // diferentes.
-  // Puede invocar la version secuencial llenarMaletaSec definida en
-  // test-maleta.c.
-  // ...
-  return 0.; // Modifique
+double llenarMaleta(double w[], double v[], int z[], int n, double maxW, int k) {
+	int pids[8];
+	int fds[8][2];
+
+	for(int i=0; i<8; i++){
+		pipe(fds[i]);
+		pids[i] = fork();
+    srandom(getUSecsOfDay()*getpid());
+
+		if(pids[i] == 0){ // hijo
+			close(fds[i][0]);
+			double res = llenarMaletaSec(w, v, z, n, maxW, k/8);
+			write(fds[i][1], &res, sizeof(int));
+			exit(0);
+		}
+		else{ // padre
+			close(fds[i][1]);
+		}
+	}
+
+	double res = 0;
+	for(int i=0; i<8; i++){
+		double res_hijo;
+		read(fds[i][0], &res_hijo, sizeof(double));
+		close(fds[i][0]);
+		waitpid(pids[i], NULL, 0);
+		if(res_hijo > res){
+			res = res_hijo;
+		}
+	}
+	return res;
 }
